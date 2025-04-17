911e6d9744a465b8f5c8170b5055d90d2e259d62
MLucasDelgado <lucasdelgado1011@gmail.com>
2025-04-17 12:04:53 -0300

    Interfaz de tareas pendientes/realizadas

--- distribuidora.c
+++ distribuidora.c
@@ -1,52 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct
 {
     int TareaID;       // Numérico autoincremental comenzando en 1000
-    char *Descripcion; //
+    char *Descripcion; 
     int Duracion;      // entre 10 – 100
 } Tarea;
 
-typedef struct
+typedef struct Nodo
 {
     Tarea T;
-    Nodo *Siguiente;
+    struct Nodo *Siguiente;
 } Nodo;
+
+Nodo *crearListaVacia();
+Nodo *crearNodo();
+void insertarNodo(Nodo **start, Nodo *Nodo);
+void pedirDatos(Nodo *Nodo, int idTarea);
+void cargarTareas(Nodo **start);
+void transferirTareas(Nodo **start, Nodo **realizadas);
 
 int main()
 {
-    Nodo *Start = NULL;
-    int idActual = 1000;
-    
-    CargarTareas(&Start, &idActual);
+    Nodo *Start = crearListaVacia(); // lista de tarea pendientes;
+    Nodo *realizadas = crearListaVacia();
 
-    
+    cargarTareas(&Start);
+    transferirTareas(&Start, &realizadas);
+
+    return 0;
 }
 
-Nodo *CrearNodo(int id){
-    Nodo *nuevoNodo = (Nodo *)malloc(sizeof(Nodo));
-    nuevoNodo->T.TareaID = id;
-    nuevoNodo->Siguiente = NULL;
-    return nuevoNodo;
-};
-
-void insertarNodo (Nodo **Start, Nodo *nodoNuevo) {
-    nodoNuevo->Siguiente = *Start;
-    *Start = nodoNuevo;
+Nodo *crearListaVacia()
+{
+    return NULL;
 }
 
-void CargarTareas (Nodo **Start, int *idActual) {
-    Nodo *nuevaTarea = CrearNodo(*idActual);
+Nodo *crearNodo()
+{
+    Nodo *NodoCreado = (Nodo *)malloc(sizeof(Nodo));
+    NodoCreado->Siguiente = NULL;
+    return NodoCreado;
+}
 
-    printf("Ingrese la descripcion: ");
-    scanf("%s", nuevaTarea->T.Descripcion);
-    
-    printf("Ingrese la duracion (10 - 100): ");
-    scanf("%d", nuevaTarea->T.Duracion);
+void insertarNodo(Nodo **start, Nodo *Nodo)
+{
+    Nodo->Siguiente = *start;
+    *start = Nodo;
+}
 
-    insertarNodo(&Start, nuevaTarea);
+void pedirDatos(Nodo *Nodo, int idTarea)
+{
+    Nodo->T.TareaID = idTarea;
+    printf("ID de la tarea: %d \n", Nodo->T.TareaID);
 
-    (*idActual++);
+    Nodo->T.Descripcion = (char *)malloc(100 * sizeof(char));
+
+    printf("Por favor ingrese la descripcion de la tarea: ");
+    fflush(stdin); // Limpia el buffer 
+    fgets(Nodo->T.Descripcion, 100, stdin);
+    // scanf(" %[^\n]", Nodo->T.Descripcion); otra forma en vez de usar el fgets
+
+    printf("Por favor ingrese la duracion '(10-100)': ");
+    scanf("%d", &Nodo->T.Duracion);
+}
+
+void cargarTareas(Nodo **Start) {
+    char continuar = 's';
+    int idTarea = 1000;
+
+    while (continuar == 's')
+    {
+        Nodo *tareaCreada = crearNodo();
+        pedirDatos(tareaCreada, idTarea);
+        insertarNodo(Start, tareaCreada);
+
+        idTarea++;
+        printf("Desea continuar? (s/n): ");
+        scanf(" %c", &continuar);
+    }
+}
+
+void transferirTareas(Nodo **start, Nodo **realizadas) {
+    Nodo * temp = *start;
+
+    while (temp != NULL)
+    {
+        printf("ID: %d\n", temp->T.TareaID);
+        printf("Descripcion: %s\n", temp->T.Descripcion);
+        printf("Duracion: %d\n", temp->T.Duracion);
+
+        char respuesta;
+        printf("Marcar esta tarea como realizada? (s/n): ");
+        scanf("% c", &respuesta);
+
+        if (respuesta == 's')
+        {
+            Nodo *tareaRealizada = temp;
+            temp = temp->Siguiente;
+        
+            tareaRealizada->Siguiente = NULL;
+            insertarNodo(realizadas, tareaRealizada);  // Insertar en la lista de realizadas
+        } else {
+            temp = temp->Siguiente;  // Solo avanzar a la siguiente tarea
+        }
+        
+    }  
 }
